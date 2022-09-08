16478143b52a81f42bb1be630ad934be33f87b73
lloydzhou <lloydzhou@qq.com>
2022-09-09 00:52:55 +0800

    使用web component模式

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { less } from 'svelte-preprocess-less';
-
-
+// import { less } from 'svelte-preprocess-less';
 
 // https://vitejs.dev/config/
-// export default defineConfig((({ command }) => {
-//   if (command == 'serve') {
-//     return {
-//       plugins: [svelte({
-//         compilerOptions: {
-//           customElement: true,
-//         }
-//       })]
-//     }
-//   }
-// })
 export default defineConfig({
   plugins: [svelte({
     compilerOptions: {
